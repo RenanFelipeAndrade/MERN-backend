3147bc43dca87dcd67aee0315e1d4cdcf4df6c98
Renan Andrade <Fel9.renan02@gmail.com>
2022-06-09 08:11:29 -0300

    Added directories skeleton

---
+++ routes/mongoEndpoint.mjs
@@ -0,0 +1,12 @@
+import express from "express";
+import { readFile } from "fs/promises";
+
+const app = express();
+
+app.get("/mongo", async (request, response) => {
+  response.send(await readFile("./home.html", "utf8"));
+});
+
+app.listen(process.env.PORT || 3000, () =>
+  console.log("App available on http://localhost:3000")
+);
